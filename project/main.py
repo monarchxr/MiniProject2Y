82e56ed6200d30dee5f202d53bce45d2cf894d31
Raunak Jais <90858989+monarchxr@users.noreply.github.com>
2024-11-25 06:49:33 +0000

    first comment grab test, worked, will label

--- project/main.py
+++ project/main.py
@@ -2,17 +2,21 @@ import instaloader
 import getpass
 from instaloader import Instaloader,ConnectionException
 from scripts.scrape_comments import scrape_comments
+import pandas as pd
+import time
 
 #logging in
 
 L = instaloader.Instaloader()
 
 username = input("Enter username: ")
-password = getpass.getpass("Enter password: ")
+# password = getpass.getpass("Enter password: ")
 
 L.context.log("debug")
 try:
-    L.login(username, password)
+    # L.login(username, password)
+    L.interactive_login(username)
+    # L.load_session_from_file(username)
     print(f"Logged in successfully as {username}")
 
 
@@ -20,5 +24,20 @@ except instaloader.exceptions.BadCredentialsException:
     print("Login failed: Invalid credentials")
 
 
-post_url = input("Enter post url: ")
-scrape_comments(L, post_url)
+shortcode = input("Enter post shortcode: ")
+# scrape_comments(L, shortcode)
+
+comments =[]
+
+# shortcode = post_url[28:39]#.split("/")[-1].strip()
+post = instaloader.Post.from_shortcode(L.context, shortcode)
+max_comments = 100
+for comment in post.get_comments():
+    comments.append(comment.text)
+    if(len(comments)>=max_comments):
+        break
+    time.sleep(2)
+
+    p = pd.DataFrame(comments, columns=["comment"])
+    p.to_csv("/workspaces/MiniProject2Y/project/data/unlabeled.csv", index=False)
+    print(f"Scraped {len(comments)} comments")
--- project/scripts/scrape_comments.py
+++ project/scripts/scrape_comments.py
@@ -1,19 +1,20 @@
 import instaloader
 import pandas as pd
-
-def scrape_comments(L, post_url, max_comments=100):
+import time
+def scrape_comments(L, shortcode, max_comments=100):
     
     L = instaloader.Instaloader()
     
     comments =[]
 
-    shortcode = post_url.split("/")[-1].strip()
+    # shortcode = post_url[28:39]#.split("/")[-1].strip()
     post = instaloader.Post.from_shortcode(L.context, shortcode)
 
     for comment in post.get_comments():
         comments.append(comment.text)
         if(len(comments)>=max_comments):
             break
+        time.sleep(2)
 
     p = pd.DataFrame(comments, columns=["comment"])
     p.to_csv("data/unlabeled.csv", index=False)
