7405952a9e8be44b8c6431acfcbcdf27bc719cd1
monarchxr <raunakjais2131@gmail.com>
2024-12-24 19:15:28 +0530

    worked on scraping again, need to diffrentiate between reel and page urls

--- project/scripts/scrape_comments.py
+++ project/scripts/scrape_comments.py
@@ -1,21 +1,48 @@
 import instaloader
 import pandas as pd
 import time
-def scrape_comments(L, shortcode, max_comments=100):
+
+def scrape_comments(session_username, post_url, output_file, max_comments=200):
     
     L = instaloader.Instaloader()
-    
-    comments =[]
 
-    # shortcode = post_url[28:39]#.split("/")[-1].strip()
-    post = instaloader.Post.from_shortcode(L.context, shortcode)
+    try:
+        L.load_session_from_file(session_username)  
+        print("Session loaded successfully!")
+    except FileNotFoundError:
+        print(f"Session file not found for user '{session_username}'")
+        return
+    except Exception as e:
+        print(f"Error loading session: {e}")
+        return
 
-    for comment in post.get_comments():
-        comments.append(comment.text)
-        if(len(comments)>=max_comments):
-            break
-        time.sleep(2)
+    comments = []
+    try:
+        shortcode = post_url[28:42].split("/")[-1].strip()
+        print(shortcode) 
+        post = instaloader.Post.from_shortcode(L.context, shortcode)
+        print(f"Fetching comments for post: {shortcode}")
 
-    p = pd.DataFrame(comments, columns=["comment"])
-    p.to_csv("data/unlabeled.csv", index=False)
-    print(f"Scraped {len(comments)} comments")
+        for comment in post.get_comments():
+            comments.append(comment.text)
+            print("Scraped", len(comments), "comments")
+            if len(comments) >= max_comments:
+                break
+            time.sleep(2)
+
+    except Exception as e:
+        print(f"Error fetching post/comments: {e}")
+        return
+
+    try:
+        df = pd.DataFrame(comments, columns=["comment"])
+        df.to_csv(output_file,mode='a',header=False ,index=False)
+        print(f"Scraped {len(comments)} comments and saved to {output_file}")
+    except Exception as e:
+        print(f"Error saving comments: {e}")
+
+if __name__ == "__main__":
+    session_username = "_monarchxr_"
+    post_url = input("Enter Instagram post URL: ")
+    output_file = r"C:\Users\rauna\OneDrive\Desktop\MiniProject2Y\project\data\unlabeled.csv" 
+    scrape_comments(session_username, post_url, output_file)
